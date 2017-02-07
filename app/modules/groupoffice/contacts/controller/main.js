36ef631af89e21b3bca3006e59a2d3df9c0be4f3
Merijn Schering <mschering@intermesh.nl>
2017-02-07 10:33:51 +0100

    Group Permissions

--- app/modules/groupoffice/contacts/controller/main.js
+++ app/modules/groupoffice/contacts/controller/main.js
@@ -7,7 +7,8 @@ GO.module('GO.Modules.GroupOffice.Contacts').controller('GO.Modules.GroupOffice.
 	'GO.Core.Services.ServerModules',
 	'GO.Modules.GroupOffice.Contacts.ContactEditor',
 	'$state',
-	function ($scope, Contact, ServerModules, ContactEditor, $state) {
+	'$mdDialog',
+	function ($scope, Contact, ServerModules, ContactEditor, $state, $mdDialog) {
 		//Will be used in child scope. We define it here so we can access
 		//the properties if needed in the future.
 		//Child scopes automatically inherit properties of the parents but
@@ -19,7 +20,7 @@ GO.module('GO.Modules.GroupOffice.Contacts').controller('GO.Modules.GroupOffice.
 			orderColumn: 't.firstName',
 			orderDirection: 'ASC'
 		});
-		
+
 		$scope.selectContact = function (contact) {
 			$state.go("contacts.contact", {contactId: contact.id});
 		};
@@ -49,8 +50,94 @@ GO.module('GO.Modules.GroupOffice.Contacts').controller('GO.Modules.GroupOffice.
 
 		};
 
-//loading done by filter collection directive
-//				$scope.store.load();			
+
+		$scope.filters = {
+			type: 'all',
+			tags: [],
+			custom: []
+		};
+
+		$scope.updateFilter = function (name, value) {
+			$scope.filters[name] = value;
+			load();
+		};
+
+		function load() {
+
+			$scope.contactStore.$loadParams.q = [];
+
+			switch ($scope.filters.type) {
+				case 'persons':
+					$scope.contactStore.$loadParams.q.push(['andWhere', {'isOrganization': false}]);
+					break;
+
+				case 'organizations':
+					$scope.contactStore.$loadParams.q.push(['andWhere', {'isOrganization': true}]);
+					break;
+
+			}
+
+			if ($scope.filters.tags.length) {
+				$scope.contactStore.$loadParams.q.push(['joinRelation', 'tags']);
+				$scope.contactStore.$loadParams.q.push(['andWhere', {'tags.id': $scope.filters.tags}]);
+			}
+
+			angular.forEach($scope.filters.custom, function (c) {
+				$scope.contactStore.$loadParams.q.push(['joinRelation', 'tags']);
+				var where = {};
+				where[c.field] = c.query;
+
+				var parts = c.field.split('.');
+				if (parts.length > 1) {
+					parts.pop();
+					var rel = parts.join('.');
+					$scope.contactStore.$loadParams.q.push(['joinRelation', rel]);
+				}
+
+				$scope.contactStore.$loadParams.q.push(['andWhere', [c.comparator, where]]);
+			});
+
+			$scope.contactStore.load();
+		}
+		;
+
+		load();
+		
+		$scope.deleteFilter = function(index) {
+			$scope.filters.custom.splice(index,1);
+			load();
+		};
+
+
+		$scope.addFilter = function () {
+			$mdDialog.show({
+				controller: ['$scope', '$mdDialog', function ($scope, $mdDialog) {
+						$scope.hide = function () {
+							$mdDialog.hide();
+						};
+
+
+						$scope.model = {field: null, query: [], comparator: 'LIKE'};
+
+						$scope.save = function () {
+							$mdDialog.hide($scope.model);
+						};
+					}],
+				templateUrl: 'modules/groupoffice/contacts/views/add-filter.html',
+
+				clickOutsideToClose: true,
+				fullscreen: true
+			})
+							.then(function (model) {
+								if (model) {
+									$scope.filters.custom.push(model);
+									load();
+								}
+
+							});
+		};
+
+
 	}]);
 
 
--- app/modules/groupoffice/contacts/controller/share.js
+++ app/modules/groupoffice/contacts/controller/share.js
@@ -1,57 +1,74 @@
 'use strict';
 
-GO.module('GO.Modules.GroupOffice.Contacts').
-				controller('GO.Modules.GroupOffice.Contacts.Controller.Share', [
-					'$scope',
-					'contact',
-					'GO.Modules.GroupOffice.Contacts.Model.ContactGroup',
-					'close',
-					'GO.Modules.GroupOffice.Users.Model.Group',
-					function ($scope, contact, ContactGroup, close, Group) {
-						
-						$scope.close = close;
-						
-						var contactGroup = new ContactGroup();
-						contactGroup.contactId = contact.id;
-						
-						$scope.store = contactGroup.getStore();
-						$scope.store.load();
-						
-						
-						
-						var groupStore = new Group().getStore({returnProperties: "id,name", limit: 10});
+GO.module('GO.Modules.GroupOffice.Contacts').controller('GO.Modules.GroupOffice.Contacts.Controller.Share', [
+	'$scope',
+	'contact',
+	'GO.Modules.GroupOffice.Contacts.Model.ContactGroup',
+	'close',
+	'GO.Modules.GroupOffice.Users.Model.Group',
+	function ($scope, contact, ContactGroup, close, Group) {
 
-						$scope.getGroups = function (input) {
-							return groupStore.load({
-								searchQuery: input
-							}).then(function (result) {								
-								return result.store.items;
-							});
-						};
-						
-						$scope.selectedGroup = null;
-						$scope.groupSearchText = "";
-						$scope.addGroup = function() {							
-							if($scope.selectedGroup) {
-								$scope.groupSearchText = "";
+		$scope.close = close;
 
-								var contactGroup = new ContactGroup();
-								contactGroup.group = $scope.selectedGroup;
-								contactGroup.contactId = contact.id;
-								contactGroup.groupId = $scope.selectedGroup.id;
-								contactGroup.save();
+		var contactGroup = new ContactGroup();
+		contactGroup.contactId = contact.id;
 
-								$scope.store.items.push(contactGroup);
-							}
-						};
-						
-						
-						$scope.deleteRecord = function(index) {
-							var record = $scope.store.items[index];
-							$scope.store.items.splice(index, 1);
-							record.deleteRecord();
-							
-							
-						};
+		$scope.store = contactGroup.getStore();
+		$scope.store.load();
 
-					}]);
+		function getGroupIds() {
+			var ids = [];
+			angular.forEach($scope.store.items, function (item) {
+				ids.push(item.groupId);
+			});
+
+			return ids;
+		}
+
+		var groupStore = new Group().getStore({returnProperties: "id,name", limit: 10});
+
+		$scope.getGroups = function (input) {
+			var p = {
+				searchQuery: input
+
+			};
+
+			var groupIds = getGroupIds();
+
+			if (groupIds.length) {
+				p['q'] = [
+					['where', ['!=', {id: groupIds}]]
+				];
+			}
+
+			return groupStore.load(p).then(function (result) {
+				return result.store.items;
+			});
+		};
+
+		$scope.selectedGroup = null;
+		$scope.groupSearchText = "";
+		$scope.addGroup = function () {
+			if ($scope.selectedGroup) {
+				$scope.groupSearchText = "";
+
+				var contactGroup = new ContactGroup();
+				contactGroup.group = $scope.selectedGroup;
+				contactGroup.contactId = contact.id;
+				contactGroup.groupId = $scope.selectedGroup.id;
+				contactGroup.save();
+
+				$scope.store.items.push(contactGroup);
+			}
+		};
+
+
+		$scope.deleteRecord = function (index) {
+			var record = $scope.store.items[index];
+			$scope.store.items.splice(index, 1);
+			record.deleteRecord();
+
+
+		};
+
+	}]);

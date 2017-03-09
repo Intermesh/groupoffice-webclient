a5dc42f9a0c337487844067401a7762e01c9beac
Merijn Schering <mschering@intermesh.nl>
2017-03-09 15:21:48 +0100

    Stats per business

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -10,7 +10,7 @@ cd build
 
 tar czf groupoffice-webclient.tar.gz *
 
-scp groupoffice-webclient.tar.gz root@amadeiro.intermesh.nl:
+#scp groupoffice-webclient.tar.gz root@amadeiro.intermesh.nl:
 scp groupoffice-webclient.tar.gz root@houtwerf.group-office.eu:/home/groupoffice7/
 
 

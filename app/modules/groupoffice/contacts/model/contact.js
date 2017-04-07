03258d736ae773f69bdff896e6e524f06b5dcf94
Merijn Schering <mschering@intermesh.nl>
2017-04-07 08:22:49 +0200

    Carddav client delete implemented

--- app/modules/groupoffice/contacts/model/contact.js
+++ app/modules/groupoffice/contacts/model/contact.js
@@ -12,7 +12,7 @@ angular.module('GO.Modules.GroupOffice.Contacts').factory('GO.Modules.GroupOffic
 
 		});
 		
-		Contact.prototype.$returnProperties = "*,photoBlob,emailAddresses,phoneNumbers,dates,addresses[*,formatted],tags[id,name,color],customfields,organizations[id,name],employees[id, name, photoBlobId],urls,groups";
+		Contact.prototype.$returnProperties = "*,photoBlob,emailAddresses,phoneNumbers,dates,addresses[*,formatted],tags[id,name,color],customfields,organizations[id,name],employees[id, name, photoBlobId],urls";
 
 		Contact.prototype.getStoreRoute = function () {
 			return 'contacts';

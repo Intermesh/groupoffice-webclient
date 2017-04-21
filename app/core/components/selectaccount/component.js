cf784995780ebb79b669fe1166dc407bafdd7010
Merijn Schering <mschering@intermesh.nl>
2017-04-21 11:00:47 +0200

    Accounts have group access by default

---
+++ app/core/components/selectaccount/component.js
@@ -0,0 +1,26 @@
+'use strict';
+
+GO.module('GO.Core').component('goSelectAccount', {
+	bindings: {
+		goCapability: '@',
+		ngModel: '='
+	},
+	controller: [
+		'$scope',
+		'GO.Core.Factories.Models.Account',
+		function ($scope, Account) {
+
+			this.$onInit = function() {
+				this.store = (new Account()).getStore({
+					capability: this.goCapability || ""
+				});				
+				this.store.load();
+			};
+			
+//			this.onOpen = function() {
+//				return this.store.load();
+//			};
+
+		}],
+	templateUrl: 'core/components/selectaccount/component.html'
+});

51210446ef06a44cf7052a166c51231cd4e37ff0
Merijn Schering <mschering@intermesh.nl>
2017-07-21 13:57:50 +0200

    Adjust search tooltip

--- app/core/directives/list-toolbar/search-button.js
+++ app/core/directives/list-toolbar/search-button.js
@@ -13,7 +13,8 @@ angular.module('GO.Core')
 					replace: false,
 					link: function (scope, element, attrs, goListToolbar) {
 						scope.enableSearch = goListToolbar.enableSearch;
+						scope.tooltip = attrs.tooltip ? attrs.tooltip : "Search";
 					},
-					template: '<md-button accesskey="s" ng-click="enableSearch()" class="md-icon-button"><md-icon>search</md-icon><md-tooltip>{{::"Search" | goT}} (Alt+s)</md-tooltip></md-button>'
+					template: '<md-button accesskey="s" ng-click="enableSearch()" class="md-icon-button"><md-icon>search</md-icon><md-tooltip>{{::tooltip | goT}} (Alt+s)</md-tooltip></md-button>'
 				};
 			}]);
